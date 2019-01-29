aab887a2d27188c78fb834a740d719e08f8a1b83
Chocoluna <47141502+Chocoluna@users.noreply.github.com>
2019-01-29 11:41:16 +0100

    Add Pixel.h & Pixel.cpp

---
+++ src/Pixel.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <Pixel.h>
+using namespace std;
+
+namespace Pixel{
+
+    int getRouge(){
+        return r;
+    }
+
+    int getVert(){
+        return g;
+    }
+
+    int getBleu(){
+        return b;
+    }
+
+};
+
+int main ()
+{
+
+    return 0;
+}
